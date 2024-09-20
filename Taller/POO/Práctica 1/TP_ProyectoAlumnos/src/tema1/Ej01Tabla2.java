c429b99aeaa62423196d0dc785912fd885486a9e
gonzaloavalos29 <gonzalo0avalos@gmail.com>
2024-09-20 13:10:54 -0300

    poo

--- Taller/POO/Práctica 1/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
+++ Taller/POO/Práctica 1/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
@@ -5,6 +5,8 @@
  */
 package tema1;
 
+import PaqueteLectura.GeneradorAleatorio;
+
 public class Ej01Tabla2 {
 
     /**
@@ -13,10 +15,14 @@ public class Ej01Tabla2 {
     public static void main(String[] args) {
         int DF=11;  
         int [] tabla2 = new int[DF]; // indices de 0 a 10
-        int i;
-        for (i=0;i<DF;i++) 
-            tabla2[i]=2*i;
-        System.out.println("2x" + "5" + "="+ tabla2[5]);
+        GeneradorAleatorio.iniciar();
+        int i = 0, num = 0;
+        while ((num != 11) && (i < DF)) {
+            num = GeneradorAleatorio.generarInt(12);
+            tabla2[i]= num*2; 
+            System.out.println("(" + i + ")" + "2x" + num + " = " + tabla2[i]);
+            i++;
+        }
 
     }
     
--- Taller/POO/Práctica 1/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
+++ Taller/POO/Práctica 1/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
@@ -3,24 +3,49 @@ package tema1;
 
 //Paso 1: Importar la funcionalidad para lectura de datos
 
+import PaqueteLectura.GeneradorAleatorio;
+import PaqueteLectura.Lector;
+
+
 
 public class Ej02Jugadores {
 
   
     public static void main(String[] args) {
         //Paso 2: Declarar la variable vector de double 
+        double[] jugadores;
         
         //Paso 3: Crear el vector para 15 double 
+        jugadores = new double[15];
         
         //Paso 4: Declarar indice y variables auxiliares a usar
+        int i, cantAlturaPromedio = 0; 
+        double alturas = 0, alturaPromedio = 0;
                 
         //Paso 6: Ingresar 15 numeros (altura), cargarlos en el vector, ir calculando la suma de alturas
+        for (i = 0; i < 15; i++) {
+            System.out.println("Ingrese altura del jugador " + i + ": ");
+            jugadores[i] = Lector.leerDouble();
+            System.out.println("Altura del jugador " + i + ": " + jugadores[i]);
+        }
         
         //Paso 7: Calcular el promedio de alturas, informarlo
+        for (i = 0; i < 15; i++) {
+            alturas += jugadores[i];
+        }
+        System.out.println("Alturas suma: " + alturas);
+        alturaPromedio = alturas / 15;
+        System.out.println("Altura promedio: " + alturaPromedio);
         
         //Paso 8: Recorrer el vector calculando lo pedido (cant. alturas que están por encima del promedio)
+        for (i = 0; i < 15; i++) {
+            if (jugadores[i] > alturaPromedio) {
+                cantAlturaPromedio += 1;
+            }
+        }
      
         //Paso 9: Informar la cantidad.
+        System.out.println("La cantidad de jugadores que están por encima de la altura promedio son: " + cantAlturaPromedio);
     }
     
 }
