dcd14e52a4f15c28e351edb775910beccd8812ea
Gonzalo <gonzalo0avalos@gmail.com>
2025-09-18 01:50:03 -0300

    act

---
+++ 2do/oo1/practicas/ejercicio2/src/main/java/ar/edu/unlp/oo1/ejercicio2/Producto.java
@@ -0,0 +1,43 @@
+package ar.edu.unlp.oo1.ejercicio2;
+
+public class Producto {
+
+	private double peso;
+	private double precioPorKilo;
+	private String descripcion;
+	
+	public Producto(String descripcion, double peso, double precioPorKilo) {
+		this.peso = peso;
+		this.precioPorKilo = precioPorKilo;
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return this.peso * this.precioPorKilo;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getPrecioPorKilo() {
+		return precioPorKilo;
+	}
+
+	public void setPrecioPorKilo(double precioPorKilo) {
+		this.precioPorKilo = precioPorKilo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
