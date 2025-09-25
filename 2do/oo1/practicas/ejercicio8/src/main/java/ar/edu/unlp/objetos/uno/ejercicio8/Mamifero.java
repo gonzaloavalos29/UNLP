05d4f2f04484f700d92ed9ade6fed14aeeda3c15
Gonzalo <gonzalo0avalos@gmail.com>
2025-09-25 00:16:57 -0300

    ej8

---
+++ 2do/oo1/practicas/ejercicio8/src/main/java/ar/edu/unlp/objetos/uno/ejercicio8/Mamifero.java
@@ -0,0 +1,81 @@
+package ar.edu.unlp.objetos.uno.ejercicio8;
+
+import java.time.LocalDate;
+
+public class Mamifero {
+	private String identificador;
+	private String especie;
+	private LocalDate fechaNacimiento;
+	private Mamifero padre;
+	private Mamifero madre;
+	
+	public Mamifero() {
+		
+	}
+	
+	public Mamifero(String identificador) {
+		this.identificador = identificador;
+	}
+	
+	public String getIdentificador() {
+		return this.identificador;
+	}
+	
+	public void setIdentificador(String id) {
+		this.identificador = id;
+	}
+	
+	public String getEspecie() {
+		return this.especie;
+	}
+	
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	public LocalDate getFechaNacimiento() {
+		return this.fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(LocalDate fecha) {
+		this.fechaNacimiento = fecha;
+	}
+	
+	public Mamifero getPadre() {
+		return this.padre;
+	}
+	
+	public void setPadre(Mamifero padre) {
+		this.padre = padre;
+	}
+	
+	public Mamifero getMadre() {
+		return this.madre;
+	}
+	
+	public void setMadre(Mamifero madre) {
+		this.madre = madre;
+	}
+	
+	public Mamifero getAbueloMaterno() {
+		return (this.madre != null) ? this.madre.getPadre() : null;
+	}
+	
+	public Mamifero getAbuelaMaterna() {
+		return (this.madre != null) ? this.madre.getMadre() : null;
+ 	}
+	
+	public Mamifero getAbueloPaterno() {
+		return (this.padre != null) ? this.padre.getPadre() : null;
+ 	}
+	
+	public Mamifero getAbuelaPaterna() {
+		return (this.padre != null) ? this.padre.getMadre() : null;
+	}
+	
+	public boolean tieneComoAncestroA(Mamifero unMamifero) {
+		return ((this.padre == unMamifero) || (this.madre == unMamifero) 
+				|| (this.padre != null && this.padre.tieneComoAncestroA(unMamifero)
+					|| (this.madre != null && this.madre.tieneComoAncestroA(unMamifero)))) ? true : false;
+	}
+}
