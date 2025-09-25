56558cd0eda09db92bc5e0b319e9331057ff8afd
Gonzalo <gonzalo0avalos@gmail.com>
2025-09-25 00:49:58 -0300

    ej9/10

---
+++ 2do/oo1/practicas/ejercicio10/src/main/java/ar/edu/unlp/objetos/uno/ejercicio10/EmpleadoJerarquico.java
@@ -0,0 +1,19 @@
+package ar.edu.unlp.objetos.uno.ejercicio10;
+
+public class EmpleadoJerarquico extends Empleado {
+	public EmpleadoJerarquico(String nombre) {
+		super(nombre);
+	}
+	
+	public double bonoPorCategoria() {
+		return 8000;
+	}
+	
+	public double montoBasico() {
+		return 45000;
+	}
+	
+	public double sueldoBasico() {
+		return super.sueldoBasico() + this.bonoPorCategoria();
+	}
+}
