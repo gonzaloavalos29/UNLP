a84a4f07666ee37e4efcea162be3e13d74d69d39
gonzaloavalos29 <gonzalo0avalos@gmail.com>
2025-10-02 13:24:53 -0300

    ej11

---
+++ 2do/oo1/practicas/ejercicio11/src/main/java/ar/edu/unlp/objetos/uno/ejercicio11/CuentaCorriente.java
@@ -0,0 +1,28 @@
+package ar.edu.unlp.objetos.uno.ejercicio11;
+
+public class CuentaCorriente extends Cuenta {
+
+	private double descubierto;
+	
+	public CuentaCorriente() {
+		super();
+		this.descubierto = 0;
+	}
+	
+	public double getDescubierto() {
+		return this.descubierto;
+	}
+	
+	public void setDescubierto(double descubierto) {
+		this.descubierto = descubierto;
+	}
+	
+	@Override
+	protected boolean puedeExtraer(double monto) {
+		return (monto <= (this.getSaldo() + this.descubierto));
+	}
+
+	
+	
+	
+}
