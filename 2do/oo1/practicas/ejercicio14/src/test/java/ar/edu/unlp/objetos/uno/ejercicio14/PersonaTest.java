e4c69fa4dba3494e618af98af11db719427904b8
gonzaloavalos29 <gonzalo0avalos@gmail.com>
2025-10-16 13:30:54 -0300

    act

---
+++ 2do/oo1/practicas/ejercicio14/src/test/java/ar/edu/unlp/objetos/uno/ejercicio14/PersonaTest.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.objetos.uno.ejercicio14;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class PersonaTest {
+	
+	
+}
